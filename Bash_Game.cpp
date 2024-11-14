cb5164b7798497ffcfef4ed1ce3443535e49d80c
YinYangDong <2736728711@qq.com>
2024-11-14 22:08:54 +0800

    power

---
+++ Bash_Game.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n=0;int m=0;
+  cin>>n>>m;
+  m++;
+  if(n%m==0) cout<<"no"<<endl;
+  else cout<<"yes"<<endl;
+  return 0;
+}
+
+/**
+ * @runId: 1912
+ * @language: C++ With O2
+ * @author: DGKL
+ * @submitTime: 2024-09-03 20:05:43
+ */
